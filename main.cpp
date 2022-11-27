2bfa03299d549d12846049961a3182985ecd06cf
Kirch <kristian2000@gmail.com>
2022-11-27 17:48:57 +0900

    first file of project

---
+++ main.cpp
@@ -0,0 +1,71 @@
+#include <iostream.h>
+#include <ctime.h>
+
+class block{
+    constructor (index, data, timestamp, hash, prevHash = " "){
+        this.index = index;
+        this.data = data;
+        this.timestamp = timestamp;
+        this.hash = calculateHash();
+        this.prevHash = prevHash;
+    }
+
+    calculateHash(){
+        
+    }
+
+}
+
+class blockchain{
+    constructor (){
+        this.chain = [];
+    }
+
+    createGenesis(){
+        return block(0, "Genesis Block", getTime(), "0")
+    }
+    
+    getTime(){
+        // current date/time based on current system
+        time_t now = time(0);
+ 
+        // convert now to string form
+        char* dt = ctime(&now);
+
+        // convert now to tm struct for UTC
+        tm *gmtm = gmtime(&now);
+        dt = asctime(gmtm);
+        return dt;
+    }
+
+    addBlock(){
+        return this.chain[this.chain.lenght + 1] = block(this.index + 1, getData(), getTime(), this.chain.lenght - 1)
+    }
+
+    getData(){
+        string fullnameT, fullnameR;
+        unsigned int amount;
+
+        cout << "Who does the transfer?  \n";
+        cin >> fullnameT;
+
+        cout << "Who is the reciever? \n";
+        cin >> fullnameR;
+
+        cout << "What is the amount being transfered? \n"
+        cin >> amount;
+
+        message();
+
+    }
+
+    message(){
+        string text;
+
+        cout << "Transfer Message \n";
+        cin >> text;
+
+        if text == " "{break;}
+        else {return text;}
+    }
+}
